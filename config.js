b82990c94124c4ce762cc0f8be5449756e4011d8
Sacha Woody <sacha.woody@yahoo.fr>
2025-03-09 20:04:25 +0100

    Add config.js for bot configuration settings

---
+++ config.js
@@ -0,0 +1,9 @@
+const config = {
+    MUDAE_ID: "Mudae_ID", // Id of Mudae Bot
+    CHANNEL_ID: "Channel_ID", // Id of the channel where Mudae is
+    SERVER_ID: "Server_ID", // Id of the server where Mudae is
+    USER_ID: "User_ID", // Id of the user who can send commands
+    BOT_TOKEN: "Your_TOKEN", // Token of ur account
+};
+
+module.exports = config;
